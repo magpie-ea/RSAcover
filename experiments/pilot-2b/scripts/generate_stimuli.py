0e183d1ef21cd1d4d4d5d98bc9db9d7a5e1d4e75
Hening Wang <hening.wang0615@gmail.com>
2025-04-13 22:37:26 +0200

    increase space between objects

--- experiments/pilot-2b/scripts/generate_stimuli.py
+++ experiments/pilot-2b/scripts/generate_stimuli.py
@@ -319,10 +319,10 @@ def main():
 
     # Generate the scatter plots
     df = generate_cluster_with_alternative(df,
-                                       min_dots=12, max_dots=16,
-                                       alt_min=6, alt_max=8,
-                                       spread=0.5, radius=13,
-                                       padding=0.8,
+                                       min_dots=10, max_dots=14,
+                                       alt_min=5, alt_max=7,
+                                       spread=0.75, radius=13,
+                                       padding=0.5,
                                        canvas_size=5, screen_size=5)
     
     # Save the updated dataframe to a new CSV file
